43df72cac1f0dadb7544b2e45fa5132718315aa1
AislinnRay <aislinn.ray.m@gmail.com>
2020-05-13 19:18:56 -0600

    fixed typo

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,16 +1,16 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Image from './Image'
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Image from "./Image";
 
 class App extends Component {
   render() {
-  return (
-    <div className="App">
-      <Image url={"https://http.cat/409"} />
-    </div>
-  );
-}
+    return (
+      <div className="App">
+        <Image url={"https://i.imgflip.com/1o12mo.jpg"} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- app-5/src/Image.js
+++ app-5/src/Image.js
@@ -1,14 +1,12 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 
-class Image extends Component{
-    render() {
-         return (
-            <div>
-             <img scr={this.props.url} />
-            <div>Error 599</div>
-            </div>
-        );
-    }
+export default class Image extends Component {
+  render() {
+    return (
+      <div>
+        <img src={this.props.url} />
+        <div>Error 599</div>
+      </div>
+    )
+  }
 }
-
-export default Image;
